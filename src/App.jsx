a97deb8b439acbaba30f828fa92c889e8b0f53e6
pokalevaishnavi <vaishnavpokale21@gmail.com>
2025-04-24 20:08:48 +0530

    portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 text-3xl font-bold text-white">
+      Hello Vaishnavi 🌸 Tailwind is working!
+    </div>
+  );
+}
+
+export default App;
