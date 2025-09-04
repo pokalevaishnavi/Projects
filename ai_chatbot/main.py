705724cbf2e60491512169499d1d8b712e5107f8
pokalevaishnavi <vaishnavpokale21@gmail.com>
2025-09-04 12:00:09 +0530

    python proj

---
+++ ai_chatbot/main.py
@@ -0,0 +1,18 @@
+# from openai import OpenAI
+
+# key = "key"
+
+
+# client = OpenAI(api_key=key)
+
+# response = client.chat.completions.create(
+#     model="gpt-4o",
+#     messages=[
+#         {"role": "user", 
+#          "content": "Write a one-sentence bedtime story about a unicorn."
+#         }
+#     ]
+
+# )
+
+# print(response.choices[0].message.content)
